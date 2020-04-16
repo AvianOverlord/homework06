7fbaeb720442b926084d68eefa8fe902c8477f1a
William Rave <wrave@gwu.edu>
2020-04-16 14:45:09 -0400

    Added in inital display code. Not working, might change approach.

--- assets/script.js
+++ assets/script.js
@@ -68,5 +68,70 @@ function addCity(cityName)
 
 function displayCity(cityName)
 {
+    $.ajax({
+      url: "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + API,
+      method: "GET"   
+    }).then(function(currentData) {
+        $.ajax({
+            url: "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName +"&appid=" + API,
+            method: "GET"
+        }).then(function (forecastData) {
+            console.log(currentData);
+            console.log(forecastData);
+            $(".currentCityName").text(cityName);
+
+            var noonArray = forecastData.list.filter(checkNoon);
+            for(var i = 0; i < 6; i++)
+            {
+                var temperature;
+                var humidity;
+                var windSpeed;
+                var UVindex;
+                var date;
+                if(i===0) //Uses today's data
+                {
+                    console.log("Breakpoint 0");
+                    var temperatureK = currentData.main.temp;
+                    temperature = (temperatureK - 273.15)*1.8 + 32;
+                    humidity = currentData.main.humidity;
+                    windSpeed = currentData.wind.speed;
+                }
+                else
+                {
+                    console.log("Breakpoint 1");
+                    var dayData = noonArray[i-1];
+                    var temperatureK = dayData.main.temp;
+                    temperature = (temperatureK - 273.15)*1.8 + 32;
+                    humidity = dayData.main.humidity;
+                    windSpeed = dayData.wind.speed;
+                    date = dayData.dt_txt.slice(0,10);
+                }
+                console.log(".displayCard[value|"+ i + "]");
+                var targetCard = $(".displayCard[value|"+ i + "]");
+                console.log("Breakpoint 2");
+                console.log(targetCard);
+                $(targetCard + " .tempDisplay").text(temperature + " &#8457;");
+                $(targetCard + " .humidDisplay").text(humidity);
+                $(targetCard + " .windDisplay").text(windSpeed + " mph");
+                if(i > 0)
+                {
+                    $(targetCard + " .cardDate").text(date);
+                }
+            }
+        })
+    });
     $(".currentCard").removeClass("hidden");
+    $(".futureCardHolder").removeClass("hidden");
+}
+
+function checkNoon(data) 
+{
+    if(data.dt_txt.includes("12:00:00"))
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
 }
